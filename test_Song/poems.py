8635031c62b2f1b394c19e08be71bfd4e8aaa584
Nineodes19 <3100863513@qq.com>
2020-06-23 23:19:39 +0800

    提交代码

--- test_Song/poems.py
+++ test_Song/poems.py
@@ -14,7 +14,7 @@ def menu():
  |------------【3】删除宋词信息-----------|
  |------------【4】修改宋词信息-----------|
  |------------【5】按照编号排序-----------|
- |------------【6】统计学生总人数---------|
+ |------------【6】统计宋词总数-----------|
  |------------【7】显示所有宋词信息-------|
  |------------【0】退出管理系统-----------|
  ======================================= 
@@ -84,7 +84,7 @@ def insert():
         else:
             temp = False
     save(poemList) # 将学生信息保存到文件
-    print("\033[1;34;47m学生信息录入完毕!!!\033[0m")
+    print("\033[1;34;47m宋词信息录入完毕!!!\033[0m")
 
 
 '''将宋词信息保存到文件'''
@@ -129,12 +129,15 @@ def search():
                     if id !="":
                         if d['id'] == id:
                             poem_query.append(d)
+
                     elif name!="":
                         if d['name'] == name:
                             poem_query.append(d)
+
                     elif author != "":
-                        if d['author']:
+                        if d['author'] == author:
                             poem_query.append(d)
+
                     show_poems(poem_query)
                     poem_query.clear()
                 inputMark = input("\033[1;34;47m>是否继续查询?(y/n):\033[0m")
@@ -155,10 +158,11 @@ def show_poems(poemList):
         #print("无效的数据")
         return
     format_title = "\033[1;34;47m---{:^6}\033[0m"
+    format_titleID = "\033[1;31;47m---{:^6}\033[0m"
     #print(format_title.format("ID", "宋词名", "词人", "内容", "思想"))
     format_data = "\t{:^6}"
     for info in poemList:
-        print(format_title.format("ID"))
+        print(format_titleID.format("ID"))
         print(format_data.format(info.get("id")))
         print(format_title.format("宋词名"))
         print(format_data.format(info.get("name")))
@@ -177,7 +181,7 @@ def show_poems(poemList):
 def delete():
     mark = True # 标记是否循环
     while mark:
-        poemId = input("\033[1;34;47m>请输入要删除的学生ID：\033[0m")
+        poemId = input("\033[1;34;47m>请输入要删除的宋词ID：\033[0m")
         if poemId !="": # 判断要删除的学生ID是否存在
             if os.path.exists(filename):
                 with open(filename, 'r') as rfile:
@@ -185,28 +189,33 @@ def delete():
             else:
                 poem_old = []
                 ifdel = False # 标记是否删除
-            if poem_old: # 如果存在学生信息
-                with open(filename, 'w+') as wfile:
-                    d = {} # 定义空字典
-                    for list in poem_old:
-                        d = dict(eval(list)) # 字符串转字典
-                        if d['id'] != poemId:
-                            wfile.write(str(d) + "\n") # 将一条信息写入文件
+            if poem_old: # 如果存在宋词信息
+                choose = input("\033[1;34;47m>请确认是否删除该信息？（y/n）\033[0m")
+                if choose == "y":
+                    with open(filename, 'w+') as wfile:
+                        d = {} # 定义空字典
+                        for list in poem_old:
+                            d = dict(eval(list)) # 字符串转字典
+                            if d['id'] != poemId:
+                                wfile.write(str(d) + "\n") # 将一条信息写入文件
+                            else:
+                                ifdel = True # 标记已经删除
+                        if ifdel:
+                            print("\033[1;34;47mID为%s的宋词信息已经被删除...\033[0m" % poemId)
                         else:
-                            ifdel = True # 标记已经删除
-                    if ifdel:
-                        print("\033[1;34;47mID为%s的学生信息已经被删除...\033[0m" % poemId)
-                    else:
-                        print("\033[1;34;47m没有找到ID为%s的学生信息...\033[0m" % poemId)
+                            print("\033[1;34;47m没有找到ID为%s的宋词信息...\033[0m" % poemId)
+                else:
+                    print("\033[1;31;47m\t操作者取消删除，删除失败！\033[0m")
+
             else:
-                 print("\033[1;34;47m不存在学生信息")
+                 print("\033[1;34;47m不存在宋词信息")
                  break
-            show() # 显示全部学生信息
+            #show() # 显示全部宋词信息
             inputMark = input("\033[1;34;47m是否继续删除？(y/n):\033[0m")
             if inputMark == "y":
                 mark = True # 继续删除
             else:
-                mark = False # 退出删除学生信息操作
+                mark = False # 退出删除宋词信息操作
 
 
 
@@ -215,24 +224,24 @@ def delete():
 
 
 def modify():
-    show() # 显示全部学生信息
+    show() # 显示全部宋词信息
     if os.path.exists(filename):
         with open(filename, 'r') as rfile:
             poem_Id = rfile.readlines()
     else:
         return
-    poemid = input("\033[1;34;47m>请输入要修改的学生ID：\033[0m")
+    poemid = input("\033[1;34;47m>请输入要修改的宋词ID：\033[0m")
     with open(filename, 'w') as wfile:
-        for student in poem_Id:
-            d = dict(eval(student))
+        for item in poem_Id:
+            d = dict(eval(item))
             if d['id'] == poemid:
                 print("\033[1;34;47m找到了该宋词，可修改信息\033[0m")
                 while True: # 输入要修改的信息
                     try:
                         d["name"] = input("\033[1;34;47m>请输入宋词名称：\033[0m")
                         d["author"] = input("\033[1;34;47m>请输入宋词的作者：\033[0m")
-                        d["python"] =input("\033[1;34;47m>请输入宋词的内容：\033[0m")
-                        d['c'] = input("\033[1;34;47m>请输入宋词的思想：\033[0m")
+                        d["content"] =input("\033[1;34;47m>请输入宋词的内容：\033[0m")
+                        d['idea'] = input("\033[1;34;47m>请输入宋词的思想：\033[0m")
                     except:
                         print("\033[1;34;47m您输入有误，请重新输入！\033[0m")
                     else:
@@ -242,7 +251,7 @@ def modify():
                 print("\033[1;34;47m修改成功\033[0m")
             else:
                 wfile.write(poems) # 将未修改的信息写入文件
-        mark = input("\033[1;34;47m是否继续修改其他学生信息？(y/n):\033[0m")
+        mark = input("\033[1;34;47m是否继续修改其他宋词信息？(y/n):\033[0m")
         if mark == "y":
             modify()
 
